fddbfcd0d3a45405ecc14950a01509c0ec083262
totallydillion <95979395+totallydillion@users.noreply.github.com>
2023-02-16 17:39:06 -0500

    Player Properties
    all of the player properties, there will probably be more as time goes on, but this is what is currently being constantly used (with the exception of attackspeed, stamina, and jumpheight, but i do plan i using those soon)

---
+++ Player/PlayerProperties.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProperties : MonoBehaviour
+{
+
+    //information made clear to the player
+    public int Health;
+    public float Stamina;
+    public int jumpHeight;
+
+    //not for showing
+    public int Radius; // how far can the player see
+
+    //stats
+    public int attackSpeed;
+    public int attackPower;
+
+}
