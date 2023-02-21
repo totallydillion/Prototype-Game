0ea57124d684171712077242a03dbeeb4a2f2ad1
totallydillion <95979395+totallydillion@users.noreply.github.com>
2023-02-20 19:47:56 -0500

    Made drops start being dropped from objects
    Instead of just destroying an object when an object is destroyed, it will now actually start dropping the drops. ObjectFunctions.cs is going to house a lot of things, so there is a lot more work to go, so I also added a ToDo at the top. At the moment there is not much, only to make the objects spread out a bit more so everything is not on top of each other, as well as have a chance to have rare drops. Currently, it is not too major because a lot of the objects in the game are basic and not "rare", but eventually there might be more objects. Like an arrow might have a broken fragment or something.

--- Objects/ObjectFunctions.cs
+++ Objects/ObjectFunctions.cs
@@ -4,32 +4,46 @@ using UnityEngine;
 
 public class ObjectFunctions : MonoBehaviour
 {
+    //todo: make it so objects are spreaded out a little bit more, as well as have a factor for rarity.
+
     ObjectProperties properties;
-    
-    
+
     void Start()
     {
-    // assign properties on the start
+        // assign properties on the start
         properties = gameObject.GetComponent<ObjectProperties>();
     }
 
     public void DeductHealth(int amount)
     {
-    // called when deducting health
-    
+        // called when deducting health
+
+        // take away the amount of health
         properties.currentHealth -= amount;
-        Debug.Log(properties.currentHealth + " | " + properties.startingHealth);
 
         if (properties.currentHealth <= 0) // if the health is less than or equal to zero, destroy object
             DestroyObject();
-            
+
         return;
     }
 
     public void DestroyObject()
     { // called when the object is destroyed 
+
+        //drop the items
+        for(int i = 0; i < properties.drops.Length; i++)
+        {
+            //for every item
+
+            //duplicate the drop at the position of the object that was destroyed
+
+            Instantiate(properties.drops[i], transform.position, Quaternion.identity);
+
+        }
+
+        //destroy the original object
         Destroy(gameObject);
-        
-        //todo ; drop something
+
+
     }
 }
