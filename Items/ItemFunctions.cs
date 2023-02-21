a17172629abfe0499ad485231d026d9a5284daaf
totallydillion <95979395+totallydillion@users.noreply.github.com>
2023-02-20 19:39:28 -0500

    Created a script for functions of items
    At the moment it is still a work in process, but eventually this script will be in charge of collecting items, placing items, etc.

---
+++ Items/ItemFunctions.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemFunctions : MonoBehaviour
+{
+    public void CollectItem(GameObject obj)
+    {
+        ItemProperties properties = obj.GetComponent<ItemProperties>();
+
+    }
+
+}
